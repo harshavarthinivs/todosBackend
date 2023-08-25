abcc015ae676cc583b692e1c0b805fa10ad75a7c
Susil A <xaviersusil@gmail.com>
2023-08-25 17:24:26 +0530

    Cors configuration added

--- src/main/java/com/example/todos/config/SecurityConfig.java
+++ src/main/java/com/example/todos/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.todos.config;
 
+import java.util.Arrays;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -8,7 +9,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import jakarta.servlet.Filter;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import lombok.RequiredArgsConstructor;
 
 @Configuration
@@ -18,13 +21,14 @@ public class SecurityConfig {
 
   private final AuthenticationProvider authenticationProvider;
   private final JwtAuthenticationFilter jwtAuthFilter;
+  private final CorsConfigurationSource configSource;
 
   @Bean
   public SecurityFilterChain getSecurityFilterChain(HttpSecurity http) throws Exception {
 
-    http.csrf(csrf -> csrf.disable())
-        .authorizeHttpRequests(authorizeRequests -> authorizeRequests.requestMatchers("/api/v1/auth/**")
-            .permitAll().anyRequest().authenticated())
+    http.cors(cors -> cors.configurationSource(configSource)).csrf(csrf -> csrf.disable())
+        .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+            .requestMatchers("/api/v1/auth/**").permitAll().anyRequest().authenticated())
         .sessionManagement(sessionManagement -> sessionManagement
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authenticationProvider(authenticationProvider)
@@ -33,4 +37,5 @@ public class SecurityConfig {
     return http.build();
   }
 
+  
 }
